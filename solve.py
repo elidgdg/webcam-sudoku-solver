0de9d42b762b88adda0e07544ed3e70d98e1ebcf
elidgdg <107950467+elidgdg@users.noreply.github.com>
2023-11-29 16:26:01 +0000

    Implement solving algorithm

---
+++ solve.py
@@ -0,0 +1,49 @@
+# find first empty cell
+def find_empty_cell(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i,j)
+    return None
+
+# check if placing num at (row, col) is valid
+def valid(board, row, col, num):
+    # check row and column
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    # check box
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+    return True
+
+def solve(board):
+    # if no empty cell, Sudoku is solved
+    if find_empty_cell(board) == None:
+        return True
+    
+    # find next empty cell
+    row, col = find_empty_cell(board)
+
+    # try numbers from 1 to 9 in empty cell
+    for num in range(1,10):
+        if valid(board, row, col, num):
+            # if valid, place num in empty cell
+            board[row][col] = num
+
+            # recursively solve the rest of the board
+            if solve(board):
+                return True
+            
+            # if current digit does not lead to solution, backtrack
+            board[row][col] = 0
+
+    # if no digit from 1 to 9 leads to a solution, return False
+    return False
