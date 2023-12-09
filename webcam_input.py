45c00b1ae5ea5d70fac0879fe09d6135db6ff765
elidgdg <107950467+elidgdg@users.noreply.github.com>
2023-12-09 11:59:34 +0000

    create webcam_input

---
+++ webcam_input.py
@@ -0,0 +1,21 @@
+import cv2
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read a frame from the webcam
+    ret, frame = cap.read()
+
+    # Display the frame
+    cv2.imshow('Webcam', frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam
+cap.release()
+
+# Close all windows
+cv2.destroyAllWindows()
