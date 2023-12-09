d73435f40760371d2b72608dc6386ce8ee518ab5
elidgdg <107950467+elidgdg@users.noreply.github.com>
2023-12-09 12:29:49 +0000

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader # PyTorch data loader module
+from torchvision import datasets # PyTorch vision module
+from torchvision.transforms import ToTensor # Transform the data into PyTorch Tensors
 import torch # PyTorch module
 import torch.nn as nn # PyTorch neural network module
 import torch.nn.functional as F # PyTorch functional module
@@ -50,40 +50,47 @@ class Net(nn.Module):
         x = self.fc2(x) # Apply the second fully connected layer
         return F.softmax(x) # Apply softmax activation to the output to get probabilities
     
-
+# Create the model, optimizer, and loss function
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # Check if a GPU is available
 model = Net().to(device) # Create the model and send it to the GPU if available
 optimizer = optim.Adam(model.parameters(), lr=0.001) # Create the optimizer
 loss_fn = nn.CrossEntropyLoss() # Create the loss function
 
+# Define the training function
 def train(epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(loaders['train']):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = loss_fn(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % 20 == 0:
+    model.train() # Set the model to training mode
+    for batch_idx, (data, target) in enumerate(loaders['train']): # Iterate over the training data
+        data, target = data.to(device), target.to(device) # Send the data to the GPU if available
+        optimizer.zero_grad() # Zero the gradients
+        output = model(data) # Forward pass through the network
+        loss = loss_fn(output, target) # Calculate the loss
+        loss.backward() # Backpropagate the loss
+        optimizer.step() # Update the weights
+        if batch_idx % 20 == 0: # Print the loss every 20 batches
             print(f'Train Epoch: {epoch} [{batch_idx * len(data)} / {len(loaders["train"].dataset)} ({100. * batch_idx / len(loaders["train"]):.0f}%)]\tLoss: {loss.item():.6f}')
 
+# Define the test function
 def test():
-    model.eval()
-    test_loss = 0
+    model.eval() # Set the model to evaluation mode
+
+    test_loss = 0 
     correct = 0
-    with torch.no_grad():
-        for data, target in loaders['test']:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += loss_fn(output, target).item()
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
-    test_loss /= len(loaders['test'].dataset)
+
+    with torch.no_grad(): # Disable gradient calculation
+        for data, target in loaders['test']: # Iterate over the test data
+            data, target = data.to(device), target.to(device) # Send the data to the GPU if available
+            output = model(data) # Forward pass through the network
+            test_loss += loss_fn(output, target).item() # Calculate the loss
+            pred = output.argmax(dim=1, keepdim=True) # Get the predicted class
+            correct += pred.eq(target.view_as(pred)).sum().item() # Calculate the number of correct predictions
+    test_loss /= len(loaders['test'].dataset) # Calculate the average loss
+
+    # Print the test results
     print(f'\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(loaders["test"].dataset)} ({100. * correct / len(loaders["test"].dataset):.0f}%)\n')
 
-if __name__ == '__main__':
+# Train and test the model
+if __name__ == '__main__': # If this file is being run directly
     for epoch in range(1, 11):
         train(epoch)
         test()
-    torch.save(model.state_dict(), 'model.pt') # Save the model parameters to a file
+    torch.save(model.state_dict(), 'model.pt') # Save the model parameters to a file 
