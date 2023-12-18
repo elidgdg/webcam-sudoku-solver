570a494e3494b1d5ddbcaa063130094053208455
elidgdg <107950467+elidgdg@users.noreply.github.com>
2023-12-18 21:26:50 +0000

    Find contours

--- image_processing.py
+++ image_processing.py
@@ -13,8 +13,14 @@ img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
 img_thresh = cv2.adaptiveThreshold(img_blur, 255, 1, 1, 11, 2)
 
+# Find the contours
+img_contours = img.copy()
+contours, hierarchy = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img_contours, contours, -1, (0, 255, 0), 3)
+
 # Display the image
 cv2.imshow('Image', img_thresh)
+cv2.imshow('Contours', img_contours)
 
 # Wait for a key press to exit
 cv2.waitKey(0)
